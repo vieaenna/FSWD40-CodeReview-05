75080ca5ad7d75d37ba517bb0314fe651028468d
Anna <anna0706@gmx.at>
2018-05-09 18:30:13 +0200

    var

---
+++ myscript.js
@@ -0,0 +1,74 @@
+var Persons = [
+                       {
+			name: 'John',
+			surname: 'Doe',
+			favoritePerformers: ["Prinz Pi", "Chakuza", "Casper"],
+			age: 25,
+   			myPhoto: "img/men1.jpg",	
+			likes: 0    // numeric property used to store likes
+			},
+
+			{
+			name: 'Jake',
+			surname: 'Deer',
+			favoritePerformers: ["Eminem", "50 Cent", "Tupac"],
+			age: 28,
+   			myPhoto: "img/men2.jpg",	
+			likes: 0 	
+			}
+
+			{
+			name: 'Markus',
+			surname: 'Wood',
+			favoritePerformers: ["Justin Bieber", "Imagine Dragons", "Chainsmokers"],
+			age: 23,
+   			myPhoto: "img/men3.jpg",	
+			likes: 0 
+			}
+
+			{
+			name: 'Steve',
+			surname: 'Thomason',
+			favoritePerformers: ["Mumford and Sons", "Joseph Arthur", "Oasis"],
+			age: 26,
+   			myPhoto: "img/men4.jpg",	
+			likes: 0 
+			}
+
+			{
+			name: 'Anna',
+			surname: 'Montana',
+			favoritePerformers: ["Mighty Oaks", "Death Cab for Cutie", "Sam Smith"],
+			age: 25,
+   			myPhoto: "img/woman1.jpg",	
+			likes: 0 
+			}
+
+			{
+			name: 'Isa',
+			surname: 'Bell',
+			favoritePerformers: ["Michael Jackson", "Frank Sinatra", "Marilyn Monroe"],
+			age: 20,
+   			myPhoto: "img/woman2.jpg",	
+			likes: 0 
+			}
+
+			{
+			name: 'Diana',
+			surname: 'Horan',
+			favoritePerformers: ["One Direction", "Miley Cyrus", "Justin Bieber"],
+			age: 23,
+   			myPhoto: "img/woman3.jpg",	
+			likes: 0 
+			}
+
+			{
+			name: 'Jules',
+			surname: 'Mayer',
+			favoritePerformers: ["John Mayer", "James Arthur", "Gavin James"],
+			age: 27,
+   			myPhoto: "img/woman4.jpg",	
+			likes: 0 
+			}
+
+];
